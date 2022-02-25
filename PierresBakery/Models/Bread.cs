d240cf7a9f1caa9fb644255a8805576bd29b7d8b
jb <jessb.epicodus@gmail.com>
2022-02-25 11:16:16 -0800

    pass: bread - get quantity

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -8,7 +8,7 @@ namespace PierresBakery.Models {
   public class Bread { 
     public int Quantity { get; }
     public Bread(int quantity) {
-      
+      Quantity = quantity;
     }
     // public int Price;
   }
