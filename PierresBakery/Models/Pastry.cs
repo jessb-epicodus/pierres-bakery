1809fb556edc2569413bfb8ede0b251e101085d9
jb <jessb.epicodus@gmail.com>
2022-02-25 13:12:46 -0800

    pass: pastry - get quantity

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -3,10 +3,12 @@ using System;
 
 //// 1 for $2 or 3 for $5
 
+
 namespace PierresBakery.Models { 
   public class Pastry { 
     public int Quantity { get; }
     public Pastry(int quantity) {
+      Quantity = quantity;
     }
   }
 }
